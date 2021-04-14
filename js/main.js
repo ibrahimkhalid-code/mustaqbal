25b23f76336c32e127f67a836788d4da5f887373
ibrahimkhalid-code <ibkh785@gmail.com>
2021-04-15 01:00:08 +0200

    mustaqbal 15/4/2021

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var bars = document.getElementById("home-header-nav-icon");
+var ul = document.getElementById("ul");
+var close = document.getElementById("home-header-nav-ulicon");
+var li = document.querySelectorAll("li");
+
+// ul.classList.add("open");
+
+bars.onclick = function width1() {
+    ul.style.left = "0px";
+    ul.style.transition = "left 1s";
+};
+
+function close1() {
+    ul.style.left = "-700px";
+    ul.style.transition = "left 2s";
+};
+console.log(ul)
